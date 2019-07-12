51a1a80c19ad2b60eeb370140ee63acf05d1bd45
Axel Neumann <neumann@cgws.de>
2019-07-12 15:36:02 +0200

    Purge unused stuff

--- src/lib/bmx7_wg_tun/wg_tun.c
+++ src/lib/bmx7_wg_tun/wg_tun.c
@@ -46,9 +46,6 @@
 
 #define CODE_CATEGORY_NAME "wg_tun"
 
-//static const struct tun_net_key ZERO_TUN_NET_KEY = { .ton = NULL };
-
-
 
 STATIC_FUNC
 int create_dsc_tlv_wg_tun(struct tx_frame_iterator *it)
@@ -65,68 +62,10 @@ int process_dsc_tlv_wg_tun(struct rx_frame_iterator *it)
 }
 
 
-
-STATIC_FUNC
-void wg_tun_cleanup(void)
+int gwg_tundev_init(void)
 {
-	/* Harry TODO */
 
-	/* The famous wtin variable */
-
-}
-
-STATIC_FUNC
-int32_t wg_tun_init(void)
-{
-	/* Initialize tunnel */
-	static const struct field_format wg_tun_adv_format[] = DESCRIPTION_MSG_WG_TUN_ADV_FORMAT;
-
-
-	/* Message handler declared in msg.h */
-	struct frame_handl tlv_handl;
-	memset(&tlv_handl, 0, sizeof(tlv_handl));
-
-	/* Register a handler for  DSC_WG_TUN */
-	tlv_handl.name = "DSC_WG_TUN";
-	tlv_handl.min_msg_size= sizeof(struct dsc_msg_wg_tun);
-	tlv_handl.fixed_msg_size = 1;
-	tlv_handl.tx_frame_handler = create_dsc_tlv_wg_tun;
-	tlv_handl.rx_msg_handler = process_dsc_tlv_wg_tun;
-	tlv_handl.msg_format = wg_tun_adv_format;
-	register_frame_handler(description_tlv_db, BMX_DSC_TLV_WG_TUN, &tlv_handl);
-
-//	register_options_array(wg_tun_options, sizeof(wg_tun_options), CODE_CATEGORY_NAME);
-
-	/* TODO HARRY */
-//	register_status_handl(sizeof(struct wg_tun_out_status), 1, wg_tun_out_status_format, ARG_TUNS, wg_tun_out_status_creator);
-
-	/* WG Tunnel Plugin initialized properly */
-	return SUCCESS;
-}
-
-/* Register Plugin and initialize */
-struct plugin* get_plugin(void)
-{
-	static struct plugin wg_tun_plugin;
-
-	memset(&wg_tun_plugin, 0, sizeof(struct plugin));
-
-	/* Assign Attributes */
-	wg_tun_plugin.plugin_name = CODE_CATEGORY_NAME;
-	wg_tun_plugin.plugin_size = sizeof(struct plugin);
-
-	/* Init */
-	wg_tun_plugin.cb_init = wg_tun_init;
-
-	/* Cleanup */
-	wg_tun_plugin.cb_cleanup = wg_tun_cleanup;
-
-	/* Register call back handler
-	 * HARRY TODO
-	 */
-//	wg_tun_plugin.cb_plugin_handler[PLUGIN_CB_SYS_DEV_EVENT] = wg_tun_dev_event_hook;
-
-	return &wg_tun_plugin;
+	return 0;
 }
 
 ///* TODO: Place them around
@@ -171,3 +110,53 @@ int wg_CnC(void)
 	return 0;
 }
 //*/
+
+
+STATIC_FUNC
+void wg_tun_cleanup(void)
+{
+	/* Harry TODO */
+}
+
+STATIC_FUNC
+int32_t wg_tun_init(void)
+{
+	/* Initialize tunnel */
+	static const struct field_format wg_tun_adv_format[] = DESCRIPTION_MSG_WG_TUN_ADV_FORMAT;
+
+
+	/* Message handler declared in msg.h */
+	struct frame_handl tlv_handl;
+	memset(&tlv_handl, 0, sizeof(tlv_handl));
+
+	/* Register a handler for  DSC_WG_TUN */
+	tlv_handl.name = "DSC_WG_TUN";
+	tlv_handl.min_msg_size= sizeof(struct dsc_msg_wg_tun);
+	tlv_handl.fixed_msg_size = 1;
+	tlv_handl.tx_frame_handler = create_dsc_tlv_wg_tun;
+	tlv_handl.rx_msg_handler = process_dsc_tlv_wg_tun;
+	tlv_handl.msg_format = wg_tun_adv_format;
+	register_frame_handler(description_tlv_db, BMX_DSC_TLV_WG_TUN, &tlv_handl);
+
+	return SUCCESS;
+}
+
+/* Register Plugin and initialize */
+struct plugin* get_plugin(void)
+{
+	static struct plugin wg_tun_plugin;
+
+	memset(&wg_tun_plugin, 0, sizeof(struct plugin));
+
+	/* Assign Attributes */
+	wg_tun_plugin.plugin_name = CODE_CATEGORY_NAME;
+	wg_tun_plugin.plugin_size = sizeof(struct plugin);
+
+	/* Init */
+	wg_tun_plugin.cb_init = wg_tun_init;
+
+	/* Cleanup */
+	wg_tun_plugin.cb_cleanup = wg_tun_cleanup;
+
+	return &wg_tun_plugin;
+}
